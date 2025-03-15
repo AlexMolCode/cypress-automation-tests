31e82736b0b46189c95708ae445f359833ae81ba
Alejandro Molina <alexmprofessional1@gmail.com>
2025-03-14 22:35:09 -0500

    test: add specs to test inventory page (#4)

--- cypress/fixtures/inventory_selectors.js
+++ cypress/fixtures/inventory_selectors.js
@@ -1,4 +1,9 @@
 export const inventorySelectors = {
+  sortProducts: '.product_sort_container',
   productsList: '.inventory_list',
-  shoppingCartIcon: '.shopping_cart_link'
+  productPrice: '.inventory_item_price',
+  productName: '.inventory_item_name',
+  shoppingCartIcon: '.shopping_cart_link',
+  addBackpackToCart: '#add-to-cart-sauce-labs-backpack',
+  removeBackpackFromCart: '#remove-sauce-labs-backpack'
 };
