533871f6b295c5f4163a1254672ae9d9ba06fd00
Alejandro Molina <alexmprofessional1@gmail.com>
2025-03-17 01:21:54 -0500

    test: add api auth tests (#7)
    Also:
    - update baseUrl for API paths
    - update yaml file to separate UI and API tests

---
+++ cypress/e2e/api/auth.cy.js
@@ -0,0 +1,63 @@
+describe("Successful Authentication", () => {
+  it("Should generate a token with valid login", () => {
+    cy.request({
+      method: "POST",
+      url: "/auth",
+      body: {
+        username: "admin",
+        password: "password123",
+      },
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      
+      // Assert token length so token itself is not exposed
+      expect(response.body.token.length).to.be.equal(15);
+
+      // Save auth token for later tests
+      cy.wrap(response.body.token, { log: false }).as("authToken");
+    });
+  });
+})
+
+describe("Failed Authentication", () => {
+  it("Should return an error message if login is invalid", () => {
+    cy.request({
+      method: "POST",
+      url: "/auth",
+      body: {
+        username: "admin",
+        password: "pass",
+      },
+    }).then((response) => {
+      expect(response.status).to.eq(200); 
+      expect(response.body).to.deep.equal({ reason: "Bad credentials" });
+    });
+  });  
+
+  it("Should return an error message if one of the login fields is missing", () => {
+    cy.request({
+      method: "POST",
+      url: "/auth",
+      body: {
+        username: "admin",
+      },
+    }).then((response) => {
+      expect(response.status).to.eq(200); 
+      expect(response.body).to.deep.equal({ reason: "Bad credentials" });
+    });
+  });
+  
+  it("Should return an error message if both user and pass are blank", () => {
+    cy.request({
+      method: "POST",
+      url: "/auth",
+      body: {
+        username: "",
+        password: "",
+      },
+    }).then((response) => {
+      expect(response.status).to.eq(200); 
+      expect(response.body).to.deep.equal({ reason: "Bad credentials" });
+    });
+  });  
+});
