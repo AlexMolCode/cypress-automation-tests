6573118d3524fb2fe61b97b0389e7c180285c723
Alejandro Molina <alexmprofessional1@gmail.com>
2025-03-21 00:21:58 -0500

    chore: set up mochawesome to generate test automation reports (#11)

--- cypress.config.js
+++ cypress.config.js
@@ -3,5 +3,18 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://restful-booker.herokuapp.com",
+    setupNodeEvents(on, config) {
+      require("cypress-mochawesome-reporter/plugin")(on);
+    },
+    reporter: "cypress-mochawesome-reporter",
+    reporterOptions: {
+      reportDir: "cypress/reports",
+      overwrite: true, // Only displays one report per run locally
+      html: true,
+      json: true,
+      charts: true, 
+      embeddedScreenshots: true,
+      inlineAssets: true  
+    }
   },
 });
