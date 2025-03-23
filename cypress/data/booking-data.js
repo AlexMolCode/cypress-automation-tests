55366b9261fa45647b7abbe0918cced1c2360c34
Alejandro Molina <alexmprofessional1@gmail.com>
2025-03-22 22:09:53 -0500

    test: add tests to catch API errors (#13)

---
+++ cypress/data/booking-data.js
@@ -0,0 +1,11 @@
+export const baseBooking = {
+  firstname: "John",
+  lastname: "Doe",
+  totalprice: 150,
+  depositpaid: true,
+  bookingdates: {
+    checkin: "2024-06-01",
+    checkout: "2024-06-10"
+  },
+  additionalneeds: "Breakfast"
+} 
