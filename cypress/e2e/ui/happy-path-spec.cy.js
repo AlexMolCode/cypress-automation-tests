8dd927d7f45862ec5ec8d3216bcf9a652c4191b0
Alejandro Molina <alexmdlnyc@gmail.com>
2025-03-23 22:03:09 -0500

    test: add happy path test for Sauce Demo

---
+++ cypress/e2e/ui/happy-path-spec.cy.js
@@ -0,0 +1,27 @@
+import { inventorySelectors } from '@/selectors/inventory-selectors';
+import { shoppingCartSelectors } from '@/selectors/shopping-cart-selectors';
+import { checkoutSelectors } from '@/selectors/checkout-selectors';
+
+describe('Full checkout test', () => {
+
+  beforeEach(() => {
+    cy.login('standard_user','secret_sauce');
+  }) 
+
+  it('Should complete a full checkout flow from cart to confirmation', () => {
+    cy.get(inventorySelectors.addBackpackToCart).click();
+    cy.get(inventorySelectors.shoppingCartIcon).click();
+    cy.get(shoppingCartSelectors.productName).should('have.text','Sauce Labs Backpack'); // Confirm it's the correct item
+    cy.get(shoppingCartSelectors.checkoutButton).click();
+    cy.get(checkoutSelectors.firstName).type('First');
+    cy.get(checkoutSelectors.lastName).type('Last');
+    cy.get(checkoutSelectors.zipCode).type('10000'); 
+    cy.get(checkoutSelectors.continueButton).click();
+    cy.get(checkoutSelectors.finishButton).click();
+    
+    cy.get(checkoutSelectors.checkoutComplete).should('have.text','Checkout: Complete!');  
+    cy.get(checkoutSelectors.thankYouText).should('have.text','Thank you for your order!');
+    cy.get(checkoutSelectors.homeButton).should('be.visible').click();
+    cy.url().should('include', '/inventory.html');   
+  })
+})
